4a4508f2b71f0ffe4e7c95de2cef47a98082fe9a
atherashraf <atherashraf@gmail.com>
2025-06-21 15:30:01 +0500

    Layer Designer

--- src/pages/LayerDesigner.tsx
+++ src/pages/LayerDesigner.tsx
@@ -1,11 +1,8 @@
 import * as React from "react";
 import MapView from "@/components/map/MapView";
 import {useParams} from "react-router-dom";
-import { useRef, useEffect} from "react";
-// import {TimeSliderHandle, IDateRange} from "@/components/map/time_slider/TimeSlider";
-// import MVTLayer from "@/components/map/layers/da_layers/MVTLayer";
-// import TimeSliderControl from "@/components/map/time_slider/TimeSliderControl";
-import {AppBar, Button, IconButton, Toolbar, Tooltip, Typography, useTheme} from "@mui/material";
+import {useRef, useEffect} from "react";
+import {AppBar,  IconButton, Toolbar, Tooltip, Typography, useTheme} from "@mui/material";
 import {getMapVM} from "@/hooks/MapVMContext";
 import SymbologyControl from "@/components/map/toolbar/controls/SymbologyControl";
 import AddIcon from "@mui/icons-material/Add"; // any icon you prefer
@@ -15,53 +12,8 @@ import AddIcon from "@mui/icons-material/Add"; // any icon you prefer
 
 const LayerDesigner = () => {
     const {layerId} = useParams();
-    // console.log("layerId", layerId);
-    //@ts-ignore
-    // const mapContainerRef: React.RefObject<MapView> = useRef();
-    // const sliderRef = useRef<TimeSliderControl | null>(null);
-    // let slider: TimeSliderControl
-    // const layerId = "2378481c-cfe1-11ed-924d-367dda4cf16d"
-    // const layerId = "6e0f2ab0-d53d-11ed-82a6-acde48001122"
-    // const layerId = "04fc474e-da80-11ed-85fe-601895253350"
-
-    // const onDateChange = (date: Date) => {
-    //     // console.log("on date change", date)
-    //     const uuid: string = "3d070b54566111eeaaaeacde48001122";
-    //     // @ts-ignore
-    //     const daLayer: MVTLayer = mapContainerRef?.current?.getMapVM().getDALayer(uuid);
-    //     if (daLayer) {
-    //         daLayer.updateTemporalData(date);
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         if (mapContainerRef.current && !sliderRef.current) {
-    //             const mapVM = mapContainerRef?.current?.getMapVM();
-    //             if (mapVM) {
-    //                 //@ts-ignore
-    //                 sliderRef.current = mapVM.addTimeSliderControl(timeSliderRef, onDateChange);
-    //                 const minDate = new Date();
-    //                 minDate.setDate(minDate.getDate() - 10);
-    //                 const s: IDateRange = {
-    //                     minDate: minDate,
-    //                     maxDate: new Date(),
-    //                 };
-    //                 setTimeout(() => timeSliderRef?.current?.setDateRange(s), 2000);
-    //                 clearInterval(interval);
-    //             }
-    //         }
-    //     }, 100);
-    //
-    //     return () => clearInterval(interval); // Cleanup the interval on unmount
-    // }, []);
 
 
-    const handleRandomClick = () => {
-        // alert("Random Click");
-        const mapVM = getMapVM()
-        mapVM?.showSnackbar("working map VM")
-    }
 
     const buttonAdded = useRef(false);
     useEffect(() => {
@@ -83,14 +35,12 @@ const LayerDesigner = () => {
     return (
         <React.Fragment>
             <MapView uuid={layerId || ''} isMap={false} theme={theme}>
-                <AppBar position="static" color="default" elevation={2}>
+                <AppBar position="static" color="secondary" elevation={2}>
                     <Toolbar variant="dense">
                         <Typography variant="h6" sx={{flexGrow: 1}}>
                             Layer Designer Toolbar
                         </Typography>
-                        <Button variant="contained" color="secondary" onClick={handleRandomClick}>
-                            Random
-                        </Button>
+
                     </Toolbar>
                 </AppBar>
             </MapView>

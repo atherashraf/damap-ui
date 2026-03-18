5482633952bd13656728fb410b0e309221ac818a
Ather Ashraf <atherashraf@gmail.com>
2026-03-19 01:04:49 +0300

    libs/damapql updates

--- src/libs/damap/components/map/layers/overlay_layers/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as AbstractOverlayLayer } from './AbstractOverlayLayer';
-export { default as IDWLayer } from './IDWLayer';
-export { default as OverlayVectorLayer } from './OverlayVectorLayer';
-export { default as SelectionLayer } from './SelectionLayer';
-export {type SelectionMode} from './SelectionLayer';
-export { default as XYZLayer } from './XYZLayer';
-export type {IXYZLayerInfo} from './XYZLayer';
-export type { IOverLayVectorInfo } from './OverlayVectorLayer';
--- src/libs/damap/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.geojson' {
-    const value: any;
-    export default value;
-}
